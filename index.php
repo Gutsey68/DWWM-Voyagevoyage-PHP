2f075acf571af38b7b35e186a3cb1e1e02ccb4d8
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-01-23 15:52:42 +0100

    début du projet

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fan club de kerim</title>
+</head>
+
+<body>
+    <h1>Quel bg ce kerim </h1>
+</body>
+
+</html>
