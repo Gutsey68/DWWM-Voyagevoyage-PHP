ef9d0b527d94b5f7cd8a993af9f01b30ba435c1b
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-01-25 21:19:58 +0100

    25/01/2024 21:19 suite de la navbar

--- views/_partial/nav.php
+++ views/_partial/nav.php
@@ -1,17 +1,14 @@
 <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-        <div class="col-lg-2">
+    <div class="container-fluid ">
             <a href="index.html">
                 <!-- mettre le logo ici -->
                 <img src="" alt="">
-                <p>Voyage<span>Voyage</span></p>
+                <p>Voyage<span class="fst-italic">Voyage</span></p>
             </a>
-        </div>
-        <div class="col-lg-3"></div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse col-lg-7 " id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-fill">
                 <li class="nav-item">
                     <a class="nav-link flex-fill" href=""><i class="fa-solid fa-compass"></i> Explorez</a>
@@ -23,21 +20,18 @@
                     <a class="nav-link flex-fill" href="">Forum</a>
                 </li>
                 <li class="nav-item">
-                    <form class="form-inline my-2 my-lg-0">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Rechercher un voyage" aria-label="Rechercher un voyage">
-                            <div class="input-group-append">
-                                <button>
+                    <form class="my-lg-0">
+                        <div class="inputgroup">
+                            <input type="text" id="navinput" placeholder="Rechercher un voyage" aria-label="Rechercher un voyage">
+                                <button id="navbutton">
                                     <i class="fa-solid fa-magnifying-glass"></i>
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </li>
-                <li class="nav-item">
-                    <button class="green-btn"><i class="fa-solid fa-user"></i> S'enregistrer / Se connecter</button>
+                <li class="nav-item ">
+                    <button class="green-btn"><i class="fa-solid fa-user" ></i>  S'enregistrer / Se connecter  </button>
                 </li>
-
             </ul>
         </div>
     </div>
