475258e0fd6da0795320e01bd9beb3d678051aa4
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-01-27 10:20:35 +0100

    27/01/2024 10:19 modification des views + suite integration page d'acceuil

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="assets/images/logo.png">
-    <title>Fan club de kerim</title>
-    <link rel="stylesheet" href="assets/style/custom.css">
-    <link rel="stylesheet" href="assets/style/bootstrap.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer">
-</head>
-
-<body>
-<!-- include du nav.php -->
-    <?php include("views/_partial/nav.php") ?><script src="assets/script/bootstrap.bundle.js"></script>
+    <?php include("views/_partial/header.php") ?>
 <!-- voyage -->
     <section id="voyage">
         <div class="container">
@@ -101,32 +84,4 @@
             </div>
         </div>
     </section>
-<!-- Footer -->
-<footer>
-    <div class="container text-center pt-5 pb-2">
-        <div class="row">
-        <div class="col-md-6 col-12">
-            <h3 class="pb-3"></i>Où nous trouver</h3>
-            <p><i class="fa-solid fa-envelope"></i> voyagevoyage@email.fr</p>
-            <p><i class="fa-solid fa-phone"></i> 07 88 48 64 97</p>
-            <p><i class="fa-solid fa-house"></i> 30 rue kerim le fou</p>
-        </div>
-        <div class="col-md-6 col-12">
-            <h3 class="pb-3">Nos résaux sociaux</h3>
-            <ul>
-                <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-x-twitter"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-pinterest-p"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-youtube"></i></a></li>
-            </ul>
-        </div>
-        </div>
-        <hr>
-        <div class="col-12">
-            <p>© 2023 VoyageVoyage Inc. Tous droits réservés</p>
-        </div>
-    </div>
-</footer>
-</body>
-</html>
+    <?php include("views/_partial/footer.php") ?>
---
+++ views/_partial/footer.php
@@ -0,0 +1,29 @@
+<!-- Footer -->
+<footer>
+    <div class="container text-center pt-5 pb-2">
+        <div id="border-bottom" class="row">
+            <div class="col-md-6 col-12 pb-3">
+                <h3 class="pb-3"></i>Où nous trouver :</h3>
+                <p><i class="fa-solid fa-envelope"></i> voyagevoyage@email.fr</p>
+                <p><i class="fa-solid fa-phone"></i> 07 88 48 64 97</p>
+                <p><i class="fa-solid fa-house"></i> 30 rue kerim le fou</p>
+            </div>
+            <div class="col-md-6 col-12">
+                <h3 class="pb-3">Nos résaux sociaux :</h3>
+                <ul>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.instagram.com/guts.sey?igsh=MW54cDV5YjNlZ3FmYQ%3D%3D&utm_source=qr" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
+                    <li><a href="" target="_blank"><i class="fa-brands fa-pinterest-p"></i></a></li>
+                    <li><a href="" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-12">
+            <p>© 2023 VoyageVoyage Inc. Tous droits réservés</p>
+        </div>
+    </div>
+</footer>
+</body>
+<script src="assets/script/bootstrap.bundle.js"></script>
+</html>
