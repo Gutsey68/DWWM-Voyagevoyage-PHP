424fe1c0e0c836d21c85dc2cd3663a532e858e4d
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-01-29 15:24:48 +0100

    maj navbar

--- views/_partial/nav.php
+++ views/_partial/nav.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid ">
-            <a href="index.php">
-                <!-- mettre le logo ici -->
-                <img src="" alt="">
-                <p>Voyage<span class="fst-italic">Voyage</span></p>
-            </a>
+        <a href="index.php">
+            <!-- mettre le logo ici -->
+            <img src="" alt="">
+            <p>Voyage<span class="fst-italic">Voyage</span></p>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,16 +21,16 @@
                 </li>
                 <li class="nav-item">
                     <form class="my-lg-0">
-                        <div class="inputgroup">
+                        <div class="inputgroup col-10">
                             <input type="text" id="navinput" placeholder="Rechercher un voyage" aria-label="Rechercher un voyage">
-                                <button id="navbutton">
-                                    <i class="fa-solid fa-magnifying-glass"></i>
-                                </button>
+                            <button id="navbutton">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
                         </div>
                     </form>
                 </li>
                 <li class="nav-item ">
-                    <button class="green-btn"><i class="fa-solid fa-user" ></i>  S'enregistrer / Se connecter  </button>
+                    <button class="green-btn"><i class="fa-solid fa-user"></i> S'enregistrer / Se connecter </button>
                 </li>
             </ul>
         </div>
