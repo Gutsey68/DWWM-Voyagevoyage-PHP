cc268d1a72662593cf59aa88e66c3e27a9e986c0
hotakmuhammad <155643748+hotakmuhammad@users.noreply.github.com>
2024-02-20 21:37:19 +0100

    Update user_controller.php
    yesss

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -109,31 +109,6 @@ class UserCtrl extends Ctrl
         $this->_arrData["arrErrors"] = $arrErrors;
         $this->afficheTpl("login");
     }
-    public function login()
-    {
-        $arrErrors = array();
-
-        /* 2. Rechercher l'utilisateur dans la BDD */
-        $strEmail     = $_POST['email'] ?? "";
-        $strPwd     = $_POST['password'] ?? "";
-
-        $objUserModel    = new UserModel;
-        $arrUser = $objUserModel->searchUser($strEmail, $strPwd);
-
-        if ($arrUser === false) {
-            /* 3. Si pas ok => Afficher un message d'erreur */
-            $arrErrors[] = "Erreur de connexion";
-        } else {
-            /* 3. Si ok => Session */
-            $_SESSION['user'] = $arrUser;
-        }
-        /* 1. Afficher le formulaire */
-        $this->_arrData["strPage"]     = "login";
-        $this->_arrData["strTitle"] = "Se connecter";
-        $this->_arrData["strDesc"]     = "Page permettant de se connecter";
-        $this->_arrData["arrErrors"] = $arrErrors;
-        $this->afficheTpl("login");
-    }
 
         /**
     * Méthode permettant de se déconnecter
